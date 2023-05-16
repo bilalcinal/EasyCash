99d4e75cccd95009408921a56651f698c8a3fd04
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-05-16 17:02:27 +0300

    Password Confirm (e-mail)

--- EasyCash.Business/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
+++ EasyCash.Business/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -8,15 +8,16 @@ namespace EasyCash.Business.ValidationRules.AppUserValidationRules;
         public AppUserRegisterValidator()
         {
 
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alani bos geçilemez");
-            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alani bos geçilemez");
-            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alani bos geçilemez");
-            RuleFor(x => x.Password).NotEmpty().WithMessage("şifre alani bos geçilemez");
-            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("şifre tekrarı alani bos geçilemez");
-            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter girin");
-            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girin");
-            RuleFor(x => x.ConfirmPassword).Equal(y =>y.Password).WithMessage("Parolar eşleşmiyor");
-            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir Email girin");
+           RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter girişi yapın");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Lütfen en az 2 karakter veri girişi yapın");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalarınız eşleşmiyor");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz");
 
         }
     }
--- EasyCash.Presentations/Controllers/RegisterController.cs
+++ EasyCash.Presentations/Controllers/RegisterController.cs
@@ -1,7 +1,9 @@
 using EasyCash.DTO.Dtos.AppUserDtos;
 using EasyCash.Entities.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 
 namespace EasyCash.Presentations.Controllers;
     public class RegisterController : Controller
@@ -24,26 +26,47 @@ namespace EasyCash.Presentations.Controllers;
     {
         if(ModelState.IsValid)
         {
+            Random random = new Random();
+            int confirmCodeSend;
+            confirmCodeSend = random.Next(100000 , 1000000);
             AppUser appUser = new AppUser()
             {  UserName = appUserRegisterDto.Username,
                Name = appUserRegisterDto.Name,
                Surname = appUserRegisterDto.Surname,
                Email = appUserRegisterDto.Email,
-               City="aaaa",
-               District="sasdfasf",
-               ImageUrl = "sCCC"
+               City = "aaaa",
+               District = "bbbb",
+               ImageUrl = "cccc",
+               ConfirmCode = confirmCodeSend
 
             };
             var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
             if(result.Succeeded)
             {
+                MimeMessage mimeMessage = new MimeMessage();
+                MailboxAddress mailboxAddressFrom = new MailboxAddress("Orospu kaan selam" , "hbilalcinal@gmail.com" );
+                MailboxAddress mailboxAddressTo = new MailboxAddress("User" , appUser.Email);
+                
+                mimeMessage.From.Add(mailboxAddressFrom);
+                mimeMessage.To.Add(mailboxAddressTo);
+
+                var bodyBuilder = new BodyBuilder();
+                bodyBuilder.TextBody = "Kayıt işlemini gerçekleştirmek için onay kodunuz: " + confirmCodeSend;
+                mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+                mimeMessage.Subject = "Eash Cash onay kodu";
+                SmtpClient client = new SmtpClient();
+                client.Connect("smtp.gmail.com", 587 ,false);
+                client.Authenticate("hbilalcinal@gmail.com", "zbbonqxhypsbxcok");
+                client.Send(mimeMessage);
+                client.Disconnect(true);
                 return RedirectToAction("Index", "ConfirmMail");
             }
             else 
             {
                 foreach(var item in result.Errors)
                 {
-                   ModelState.AddModelError(" " , item.Description);
+                    ModelState.AddModelError("", item.Description);
                 }
             }
         }
